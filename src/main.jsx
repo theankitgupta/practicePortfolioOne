8d096abd2c2246b46d84206fc925c7e49e517bb8
Ankit Gupta <theankitguptaa@gmail.com>
2025-12-01 01:55:52 +0530

    2nd Commit- Setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+// Root DOM Element - Grabs the root container from index.html
+const rootElement = document.getElementById('root');
+
+// Defensive check (useful in production builds)
+if (!rootElement) {
+  throw new Error('Root element not found. Check your index.html file for <div id="root"></div>');
+}
+
+// React Root Initialization 
+const root = createRoot(rootElement);
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    {/* Wrap BrowserRouter for app-wide routing */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
